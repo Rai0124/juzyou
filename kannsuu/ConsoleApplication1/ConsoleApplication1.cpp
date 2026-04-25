1ff216167afab76ae58ba9f4354b16b840bafeca
Rai0124 <rw0124@icloud.com>
2026-04-25 16:22:07 +0900

    kannsuu

---
+++ kannsuu/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+﻿#include<stdio.h>
+
+int f(int number, int number2);
+
+//メイン関数
+void f()
+{
+	static int answer = 0;
+	answer = answer + 1;
+	printf("関数 f がよびだされました。 answer は [%d] です\n", answer);
+}
+
+int main()
+{
+	//関数 f を使う
+	f();
+
+
+	return 0;
+}
