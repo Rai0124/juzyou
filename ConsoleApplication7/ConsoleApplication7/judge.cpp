8af01c73a0e57c806de8c215ab65d01f0278bb30
XPS\ktc <>
2026-05-30 09:03:03 +0900

    a

---
+++ ConsoleApplication7/ConsoleApplication7/judge.cpp
@@ -0,0 +1,49 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include "judge.h"
+
+
+int f(int number, float value)
+{
+	int t = (int)time(nullptr);
+	int answer = t % 10;
+	printf("関数 f が呼び出されました。\n", answer);
+
+	return answer;
+}
+
+
+
+int main()
+{
+
+	int answer = f(1,1.0f);
+
+	while (true)
+	{
+		int input;
+		scanf_s("%d", &input);
+
+
+		if (answer == input)
+		{
+			printf("ゲームクリア\n");
+		}
+		else if (answer < input)
+		{
+			printf("大きい!\n");
+		}
+		else if (answer > input)
+		{
+			printf("小さい!\n");
+		}
+
+		if (answer == input)
+		{
+			break;
+		}
+
+	}
+	return 0;
+}
