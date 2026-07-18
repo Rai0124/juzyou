46ba14836b5066a274f142b650243fac7a51b0f6
Rai0124 <rw0124@icloud.com>
2026-07-18 16:24:28 +0900

    ンナ

---
+++ ConsoleApplication20/ConsoleApplication20/ConsoleApplication20.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+
+
+int main()
+{
+	std::vector<float>vec = { 1,2,3,4,5,6,7,8,9,10 };
+
+	std::sort(vec.begin(), vec.end());
+
+
+	for (std::vector<float>::iterator it = vec.begin();
+		it != vec.end();
+		it++)
+	{
+		std::cout << *it << "  ";
+	}
+
+
+}
